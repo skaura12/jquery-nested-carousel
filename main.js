100e7330ba9e6f382570e30e91888896b89bdb9f
Sandeep Kaura <sandeep.kaura@comprotechnologies.com>
2016-05-10 19:19:57 +0530

    Updates

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-(function(){
-    var nestedTimeline = function(params){
+(function($){
+    var defaults = {
+        innerNodeDistance: 20,
+        outerNodeDistance: 50
+    };
 
-    }
-})();
+    $.fn.nestedTimeline = function(options){
+        var settings = $.extend({},defaults,options);
+        return this.each(function(){
+            loadTimeline(this);
+        });
+
+        function loadTimeline(timelineWrapper){
+            timelineWrapper.find("ol > li").each(function(){
+
+            });
+        }
+    };
+
+
+
+
+
+
+})(jQuery);
