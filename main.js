13f3de50a69f3baae33f6fabbfbdfce02f0088e8
Sandeep Kaura <sandeep.kaura@comprotechnologies.com>
2016-05-11 19:25:44 +0530

    updates

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 (function($){
-    var defaults = {
-        innerNodeDistance: 20,
-        outerNodeDistance: 50
-    };
+    var defaults = {};
 
     $.fn.nestedTimeline = function(options){
         var settings = $.extend({},defaults,options);
-        return this.each(function(){
-            loadTimeline(this);
+        this.each(function(){
+            loadTimeline($(this));
         });
 
         function loadTimeline(timelineWrapper){
-            timelineWrapper.find("ol > li").each(function(){
+            var outerNodeCollection = timelineWrapper.find("ol > li");
 
+            timelineWrapper.find(".ns-timeline-navigation a.prev").on("click",function(){
+                if(timelineWrapper.find("ol > li.selected").prev().length){
+                    timelineWrapper.find("ol > li.selected").removeClass("selected").prev().addClass("selected");
+
+                }
             });
+
+            timelineWrapper.find(".ns-timeline-navigation a.next").on("click",function(){
+                timelineWrapper.find("ol > li.selected").removeClass("selected").next().addClass("selected");
+            });
+
+
+            updateOuterNodeWidth(outerNodeCollection);
+        }
+
+        function updateOuterNodeWidth(listCollection) {
+            listCollection.each(function(){
+                var innerNodeCount  = $(this).find("a").length;
+                console.log(innerNodeCount);
+            })
         }
     };
 
