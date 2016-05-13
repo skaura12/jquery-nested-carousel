6f6d931ecbe5eb464f500858280b04d54ab3df90
Sandeep Kaura <sandeep.kaura@comprotechnologies.com>
2016-05-13 18:59:56 +0530

    Updates

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
 (function($){
-    var defaults = {};
+    var pluginName = 'nestedTimeline';
 
-    $.fn.nestedTimeline = function(options){
-        var settings = $.extend({},defaults,options);
-        this.each(function(){
-            loadTimeline($(this));
-        });
+    function Plugin(element,options){
+        this.$ele = element;
+        this.options = $.extend({}, $.fn.defaults,options);
+        this.init();
+    }
 
-        function loadTimeline(timelineWrapper){
-            var totalContentWidth = 0,containerWidth,
-                timelineComponents = {},
-                selectedOuterNode = timelineWrapper.find(".list > ol > li.selected"),
+    function getTranslateValue(obj){
+        var translateValue,currentTranslateValue;
+
+        translateValue = obj.css("transform");
+
+        if( translateValue.indexOf('(') >=0 ) {
+            translateValue = translateValue.split('(')[1];
+            translateValue = translateValue.split(')')[0];
+            translateValue = translateValue.split(',');
+            currentTranslateValue = Number(translateValue[4]);
+        } else {
+            currentTranslateValue = 0;
+        }
+
+        return currentTranslateValue;
+    }
+
+    function resetListItems(listCollection){
+        listCollection.each(function () {
+            $(this).removeClass("selected");
+            $(this).find("a").removeClass("selected");
+            $(this).find("div.active").remove();
+        })
+    }
+
+    Plugin.prototype = {
+        init: function(){
+            var timelineComponents = {},
+                selectedOuterNode = this.$ele.find(".list > ol > li.selected"),
                 selectedInnerNode;
 
-            timelineComponents["outerNodeList"] = timelineWrapper.find(".list > ol >li");
+            timelineComponents["outerNodeList"] = this.$ele.find(".list > ol >li");
 
             if(!selectedOuterNode.length){
                 selectedOuterNode = $(timelineComponents["outerNodeList"][0]);
@@ -28,115 +53,90 @@
             selectedOuterNode.addClass("selected");
             selectedInnerNode.addClass("selected");
             selectedOuterNode.append("<div class='active'>"+selectedInnerNode.data('content')+"</div");
-
-            timelineComponents["outerNodeList"].each(function(){
-                totalContentWidth += $(this).outerWidth();
+            this.totalContentWidth = 0;
+            this.$ele.find(".list > ol >li").each(function(){
+                this.totalContentWidth += $(this).outerWidth();
             });
-            containerWidth = timelineWrapper.find(".list ol").width();
+            this.containerWidth = this.$ele.find(".list ol").width();
+            this.attachEvents();
+        },
+        attachEvents: function(){
+            var self = this;
 
-
-            console.log(totalContentWidth);
-            timelineWrapper.find(".ns-timeline-navigation a.prev").on("click",function(){
+            self.$ele.find(".ns-timeline-navigation a.prev").on("click",function(){
                 var currentTranslateValue,contentWidthTillSelectedListItem = 0,contentWidthTillPrevOfSelected,translationValue;
-                if(timelineWrapper.find("ol > li.selected").prev().length){
+                if(self.$ele.find("ol > li.selected").prev().length){
 
-                    currentTranslateValue = getTranslateValue(timelineWrapper.find(".list ol"));
+                    currentTranslateValue = getTranslateValue(self.$ele.find(".list ol"));
 
-                    timelineWrapper.find(".list > ol > li").each(function(){
+                    self.$ele.find(".list > ol > li").each(function(){
                         contentWidthTillSelectedListItem += $(this).outerWidth();
                         if($(this).hasClass("selected")){
                             return false;
                         }
                     });
-                    contentWidthTillPrevOfSelected = contentWidthTillSelectedListItem - timelineWrapper.find("ol > li.selected").outerWidth();
+                    contentWidthTillPrevOfSelected = contentWidthTillSelectedListItem - self.$ele.find("ol > li.selected").outerWidth();
 
-                    if((contentWidthTillPrevOfSelected + currentTranslateValue) < timelineWrapper.find("ol > li.selected").prev().outerWidth()){
+                    if((contentWidthTillPrevOfSelected + currentTranslateValue) < self.$ele.find("ol > li.selected").prev().outerWidth()){
                         //translation required
-                        if( (containerWidth -currentTranslateValue - contentWidthTillPrevOfSelected) < contentWidthTillPrevOfSelected){
+                        if( (self.containerWidth -currentTranslateValue - contentWidthTillPrevOfSelected) < contentWidthTillPrevOfSelected){
                             translationValue = containerWidth -currentTranslateValue - contentWidthTillPrevOfSelected;
                         }else{
                             translationValue = -currentTranslateValue;
                         }
                         $(".list ol").css("transform", "translateX(" + (currentTranslateValue + translationValue) + "px)");
                     }
-                    timelineWrapper.find("ol > li.selected > div.active").remove();
-                    timelineWrapper.find("ol > li.selected a.selected").removeClass("selected");
-                    timelineWrapper.find("ol > li.selected").removeClass("selected").prev().addClass("selected").append("<div class='active'>"+timelineWrapper.find("ol > li.selected a").data('content')+"</div");
-                    $(timelineWrapper.find("ol > li.selected a")[0]).addClass("selected");
+                    self.$ele.find("ol > li.selected > div.active").remove();
+                    self.$ele.find("ol > li.selected a.selected").removeClass("selected");
+                    self.$ele.find("ol > li.selected").removeClass("selected").prev().addClass("selected").append("<div class='active'>"+self.$ele.find("ol > li.selected a").data('content')+"</div");
+                    $(self.$ele.find("ol > li.selected a")[0]).addClass("selected");
 
                 }
             });
 
-            timelineWrapper.find(".ns-timeline-navigation a.next").on("click",function(){
+            self.$ele.find(".ns-timeline-navigation a.next").on("click",function(){
                 var contentWidthTillSelectedListItem = 0,translation,currentTranslateValue,translateValue,widthFromOriginTillSelected;
 
-                if(timelineWrapper.find("ol > li.selected").next().length){
-                    timelineWrapper.find(".list > ol > li").each(function(){
+                if(self.$ele.find("ol > li.selected").next().length){
+                    self.$ele.find(".list > ol > li").each(function(){
                         contentWidthTillSelectedListItem += $(this).outerWidth();
                         if($(this).hasClass("selected")){
                             return false;
                         }
                     });
-                    currentTranslateValue = getTranslateValue(timelineWrapper.find(".list ol"));
-
-                    console.log("contentWidthTillSelectedListItem",contentWidthTillSelectedListItem);
-                    console.log("currentTranslateValue",currentTranslateValue);
-
+                    currentTranslateValue = getTranslateValue(self.$ele.find(".list ol"));
                     widthFromOriginTillSelected = contentWidthTillSelectedListItem + currentTranslateValue;
-                    if((widthFromOriginTillSelected+ timelineWrapper.find("ol > li.selected").next().outerWidth()) > containerWidth) {
-                        if ((totalContentWidth + currentTranslateValue - containerWidth) > widthFromOriginTillSelected) {
+                    if((widthFromOriginTillSelected+ self.$ele.find("ol > li.selected").next().outerWidth()) > containerWidth) {
+                        if ((self.totalContentWidth + currentTranslateValue - containerWidth) > widthFromOriginTillSelected) {
                             translation = widthFromOriginTillSelected;
                         } else {
-                            translation = (totalContentWidth + currentTranslateValue - containerWidth);
+                            translation = (self.totalContentWidth + currentTranslateValue - containerWidth);
                         }
                         console.log("translation", translation);
                         $(".list ol").css("transform", "translateX(" + (currentTranslateValue - translation) + "px)");
                     }
-                    timelineWrapper.find("ol > li.selected > div.active").remove();
-                    timelineWrapper.find("ol > li.selected a.selected").removeClass("selected");
-                    timelineWrapper.find("ol > li.selected").removeClass("selected").next().addClass("selected").append("<div class='active'>"+timelineWrapper.find("ol > li.selected a").data('content')+"</div");
-                    $(timelineWrapper.find("ol > li.selected a")[0]).addClass("selected");
+                    self.$ele.find("ol > li.selected > div.active").remove();
+                    self.$ele.find("ol > li.selected a.selected").removeClass("selected");
+                    self.$ele.find("ol > li.selected").removeClass("selected").next().addClass("selected").append("<div class='active'>"+self.$ele.find("ol > li.selected a").data('content')+"</div");
+                    $(self.$ele.find("ol > li.selected a")[0]).addClass("selected");
                 }
             });
 
-            timelineWrapper.find(".list ol").on("click","a",function(event){
+            self.$ele.find(".list ol").on("click","a",function(event){
                 console.log(event);
                 event.preventDefault();
                 $(event.delegateTarget).find("li.selected a.selected").removeClass("selected");
                 $(event.delegateTarget).find("li.selected div.active").html($(event.target).addClass("selected").data("content"));
             })
         }
-
-        function getTranslateValue(obj){
-            var translateValue,currentTranslateValue;
-
-            translateValue = obj.css("transform");
-
-            if( translateValue.indexOf('(') >=0 ) {
-                translateValue = translateValue.split('(')[1];
-                translateValue = translateValue.split(')')[0];
-                translateValue = translateValue.split(',');
-                currentTranslateValue = Number(translateValue[4]);
-            } else {
-                currentTranslateValue = 0;
-            }
-
-            return currentTranslateValue;
-        }
-
-        function resetListItems(listCollection){
-            listCollection.each(function () {
-                $(this).removeClass("selected");
-                $(this).find("a").removeClass("selected");
-                $(this).find("div.active").remove();
-            })
-        }
-
     };
-
-
-
-
-
-
+    $.fn[pluginName].defaults = {};
+    $.fn[pluginName] = function(options){
+        var settings = $.extend({},defaults,options);
+        return this.each(function(){
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName,new Plugin( $(this), options ));
+            }
+        });
+    };
 })(jQuery);
